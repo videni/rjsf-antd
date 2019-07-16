42afbf17e38924fdae191c89d9a842c63c2f1170
Vidy Videni <vidy.videni@gmail.com>
2019-07-16 17:02:43 +0800

    Add Webpack production confinguration

---
+++ config/webpack.dist.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { basicConfig, rules, plugins } = require('./config');
+const webpack = require('webpack');
+
+const config = {
+  mode: 'production',
+  cache: true,
+  entry: path.join(__dirname, '../src/index.ts'),
+  output: {
+    path: path.join(__dirname, '../dist'),
+    publicPath: '../dist/',
+    filename: 'rjsf-antd.js',
+    library: 'JSONSchemaFormAntd',
+    libraryTarget: 'umd',
+  },
+  devtool: 'source-map',
+  module: {
+    rules,
+  },
+  plugins,
+};
+
+module.exports = merge(basicConfig, config, {
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      },
+    }),
+  ],
+});
